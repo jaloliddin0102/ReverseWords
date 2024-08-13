10dc33294f13c430748b424b9af198dfb7391479
Jaloliddin <jaloliddinxolmirzayev2@gmail.com>
2024-08-13 17:21:53 +0500

    add comment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string s = "Let's take LeetCode contest";
+Console.WriteLine(ReverseWords(s));
+    static string ReverseWords(string s) {
+        string[]array = s.Split(new char[]{' '});
+        string answer = "";
+        for (int i = 0; i < array.Length; i++) 
+            {
+                for (int j = s.Length - 1; j >= 0; j--)
+                {
+                        answer += array[i][j];
+                }
+                if (i!=array.Length-1)
+                answer += " ";
+            }
+        return answer;
+    }
